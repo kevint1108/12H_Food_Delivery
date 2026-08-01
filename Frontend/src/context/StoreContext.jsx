a399b161d0e843078233433f9e35eee67d1437e3
Binh Tang <terikn1107@gmail.com>
2026-07-31 22:39:24 -0700

    Connect frontend and admin to production API

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://12h-food-delivery.vercel.app/"
+    const url = "https://12-h-food-delivery-6zik.vercel.app/"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
     const [search, setSearch] = useState("");
