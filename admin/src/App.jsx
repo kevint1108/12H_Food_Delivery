30f3af03565c870c2afc67c9fe9275664576c3d3
Binh Tang <terikn1107@gmail.com>
2026-07-31 22:44:50 -0700

    Connect frontend and admin to production API

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,7 +20,6 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path="/" element={<Navigate to="/list" replace />}/>
           <Route path="/add" element={<Add url={url}/>}/>
           <Route path="/list" element={<List url={url}/>}/>
           <Route path="/orders" element={<Orders url={url}/>}/>
