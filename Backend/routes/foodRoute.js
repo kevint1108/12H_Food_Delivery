8a76feaeca28eaec337f20bd3f485472893832fe
Binh Tang <terikn1107@gmail.com>
2026-08-02 17:55:31 -0700

    Sync full deployment fix

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -4,7 +4,14 @@ import multer from "multer";
 
 const foodRouter = express.Router();
 
-const upload = multer({ storage: multer.memoryStorage() });
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
